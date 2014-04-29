a95491b80ea0371586de537a35936d36815c9365
Kamil <Kamil@Kamil-Komputer>
2014-04-29 16:06:43 +0200

    Gotowa wersja

--- Config.php
+++ Config.php
@@ -1,8 +1,17 @@
 <?php
 
-$_db_type='mysql';
-$_db_host='localhost';
-$_db_name='kzielinski';
-$_db_port='3306';
-$_db_user='kzielinski';
-$_db_pass='123456';
+if (0) {
+    $_db_type = 'mysql';
+    $_db_host = 'localhost';
+    $_db_name = 'kzielinski';
+    $_db_port = '3306';
+    $_db_user = 'kzielinski';
+    $_db_pass = '123456';
+} else {
+    $_db_type = 'pgsql';
+    $_db_host = 'localhost';
+    $_db_name = 'kzielinski';
+    $_db_port = '5432';
+    $_db_user = 'kzielinski';
+    $_db_pass = '123456';
+}
